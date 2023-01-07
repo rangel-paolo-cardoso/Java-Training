666eedbcced5e7e48758081fe9068938adefdd89
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-07 12:36:21 -0300

    Exercise 4 - For, While and Do While - Loiane Groner - done.

---
+++ Loiane-Grone/for-while-do-while-exercises/src/Exercise4.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Population A has 80,000 inhabitants.");
+        System.out.println("Its annual growth rate is 3%.");
+        double populationA = 80_000.00;
+        double annualGrowthRateA = 3 / 100.00;
+
+        System.out.println("Population B has 200,000 inhabitants.");
+        System.out.println("Its annual growth rate is 1.5%");
+        double populationB = 200_000.00;
+        double annualGrowthRateB = 1.5 / 100.00;
+
+        int years = 0;
+
+        while (populationA < populationB) {
+            populationA += populationA * annualGrowthRateA;
+            populationB += populationB * annualGrowthRateB;
+            years += 1;
+        }
+
+        System.out.println(
+            "It will take "
+            + years
+            + " years for population A become larger than population B"
+        );
+
+        sc.close();
+    }
+}
