546ab96b92e960607ef769ed0c4aecf4c3463baf
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-07 13:11:31 -0300

    Exercise 7 - For, While and Do While - Loiane Groner - done.

---
+++ Loiane-Grone/for-while-do-while-exercises/src/Exercise7.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int theBiggest = 0;
+
+        for (int count = 1; count < 6; count++) {
+            System.out.print("Enter the " + count + "º number: ");
+            int number = sc.nextInt();
+            if (number > theBiggest) {
+                theBiggest = number;
+            }
+        }
+
+        System.out.println("The biggest number is " + theBiggest);
+
+        sc.close();
+    }
+}
