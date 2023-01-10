bd85429fafec8027a8a4e865c3c39edd035e00b5
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-10 20:31:30 -0300

    Exercise 2 - Java Explorer

--- Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
+++ Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -3,5 +3,9 @@ package duke.choice;
 public class ShopApp {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to Duke Choice Shop!");
+
+        Customer c1 = new Customer();
+        c1.name = "Pinky";
+        System.out.println("Welcome " + c1.name);
     }
 }
