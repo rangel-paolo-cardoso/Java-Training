474b17f6b7bb5b8485816e6a5928945d089cbb6c
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-11 10:48:37 -0300

    Exercise 8 - Become a Java Ace

---
+++ Become-Java-Ace/great-exercises/src/Exercise8.java
@@ -0,0 +1,16 @@
+public class Exercise8 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(printResult());
+    }
+
+    public static String printResult() {
+        String pattern = "";
+        for (int i = 0; i < 4; i++) {
+            for (int j = i; j >= 0; j--) {
+                pattern += "*";
+            }
+            pattern += "\n";
+        }
+        return pattern;
+    }
+}
