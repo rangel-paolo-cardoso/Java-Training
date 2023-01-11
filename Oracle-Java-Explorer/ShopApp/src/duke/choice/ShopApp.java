0b1f60d9ffd60d7fbab896f43924f183503b1f0d
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-10 21:07:33 -0300

    Exercise 3 - Java Explorer

--- Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
+++ Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -1,11 +1,37 @@
 package duke.choice;
 
+import java.util.Locale;
+
 public class ShopApp {
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        double tax = 0.2;
+        double total = 0.0;
+
         System.out.println("Welcome to Duke Choice Shop!");
 
         Customer c1 = new Customer();
         c1.name = "Pinky";
         System.out.println("Welcome " + c1.name);
+
+        Clothing item1 = new Clothing();
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
+        
+        Clothing item2 = new Clothing();
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.5;
+        item2.size =  "S";
+
+        System.out.println(
+            item1.description + "," + item1.price + "," + item1.size
+        );
+        System.out.println(
+            item2.description + "," + item2.price + "," + item2.size
+        );
+
+        total = (item1.price + item2.price + tax) * (1 + tax);
+        System.out.println("Total = " + total);
     }
 }
