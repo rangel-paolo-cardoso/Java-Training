c01f09809ad7278548c2e1adc9e70e70bab0352b
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-11 20:05:14 -0300

    Exercise 4 - Java Explorer

--- Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
+++ Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -12,26 +12,42 @@ public class ShopApp {
 
         Customer c1 = new Customer();
         c1.name = "Pinky";
+        c1.size = "S";
+
         System.out.println("Welcome " + c1.name);
 
         Clothing item1 = new Clothing();
         item1.description = "Blue Jacket";
         item1.price = 20.9;
         item1.size = "M";
-        
+
         Clothing item2 = new Clothing();
         item2.description = "Orange T-Shirt";
         item2.price = 10.5;
-        item2.size =  "S";
+        item2.size = "S";
 
         System.out.println(
-            item1.description + "," + item1.price + "," + item1.size
-        );
+                item1.description + "," + item1.price + "," + item1.size);
         System.out.println(
-            item2.description + "," + item2.price + "," + item2.size
-        );
+                item2.description + "," + item2.price + "," + item2.size);
 
         total = (item1.price + item2.price + tax) * (1 + tax);
         System.out.println("Total = " + total);
+
+        int measurement = 3;
+
+        switch (measurement) {
+            case 1, 2, 3:
+                c1.size = "S";
+                break;
+            case 4, 5, 6:
+                c1.size = "M";
+                break;
+            case 7, 8, 9:
+                c1.size = "L";
+                break;
+            default:
+                c1.size = "X";
+        }
     }
 }
