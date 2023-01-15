99dc9982f5a2a4147a6d529b2b1692f496817f2e
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-14 22:35:21 -0300

    Exercise 25 - For, While and Do While - Loiane Groner - done

---
+++ Loiane-Grone/for-while-do-while-exercises/src/Exercise25.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise25 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many students are there to calculate the average age? ");
+        int numberOfPeople = sc.nextInt();
+        double ageAverage = 0.0;
+
+        int count = 0;
+        while (count < numberOfPeople) {
+            System.out.print("Enter the age of the " + (count + 1) + "º person: ");
+            int age = sc.nextInt();
+            ageAverage += ageAverage;
+            count += 1;
+        }
+
+        ageAverage /= numberOfPeople;
+
+        if (ageAverage > 0 && ageAverage < 26) {
+            System.out.println("The class is young");
+        } else if (ageAverage < 61) {
+            System.out.println("The class is adult");
+        } else {
+            System.out.println("The class is elderly");
+        }
+
+        sc.close();
+    }
+}
