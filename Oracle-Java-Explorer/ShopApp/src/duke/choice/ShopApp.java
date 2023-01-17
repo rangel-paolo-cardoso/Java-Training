2655b43f083a1ee2f1d1043fbdbef8e85858f78e
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-16 22:07:58 -0300

    Exercise 13 - Java Explorer

--- Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
+++ Oracle-Java-Explorer/ShopApp/src/duke/choice/ShopApp.java
@@ -1,6 +1,11 @@
 package duke.choice;
 
-import java.util.Arrays;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.ServerConfiguration;
+import io.helidon.webserver.WebServer;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import java.util.Locale;
 
 public class ShopApp {
@@ -9,50 +14,24 @@ public class ShopApp {
 
         System.out.println("Welcome to Duke Choice Shop!");
 
-        Customer c1 = new Customer("Pinky", 3);
-
-        System.out.println("Minimum price: " + Clothing.MIN_PRICE);
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
-
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
-
         Clothing item3 = new Clothing("Green Scarf", 5, "S");
-
         Clothing item4 = new Clothing("Blue T-Shirt", 10.5, "S");
 
-        Clothing[] items = new Clothing[4];
-        items[0] = item1;
-        items[1] = item2;
-        items[2] = item3;
-        items[3] = item4;
+        Clothing[] items = { item1, item2, item3, item4 };
 
-        int measurement = 3;
-        c1.setSize(measurement);
-        c1.addItems(items);
-
-        System.out.println("Customer is " + c1.getName() + ", " + c1.getSize() + ", " + c1.getTotalClothingCost());
-
-        Arrays.sort(c1.getItems());
-
-        for (Clothing item : c1.getItems()) {
-            System.out.println("Item " + item);
-        }
-
-        int average = 0;
-        int count = 0;
-
-        for (Clothing item : c1.getItems()) {
-            if (item.getSize().equals("L")) {
-                count++;
-                average += item.getPrice();
-            }
-        }
-
-        if (count == 0) {
-            System.out.println("Count is zero, so we can't proceed with the division");
-        } else {
-            average = average / count;
-            System.out.println("Average price " + average + ", Count " + count);
+        try {
+            ItemList list = new ItemList(items);
+            Routing routing = Routing.builder()
+                    .get("/items", list).build();
+            ServerConfiguration config = ServerConfiguration.builder()
+                    .bindAddress(InetAddress.getLocalHost())
+                    .port(8888).build();
+            WebServer ws =WebServer.create(config, routing);
+            ws.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         }
     }
 }
