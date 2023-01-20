2359598531942d3c3176f607be167153b3fab4c0
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-20 20:45:22 -0300

    Exercise 37 - Geek University done.

---
+++ Geek-University/section5/src/Exercise37.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise37 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String numbers = "";
+
+        for (int count = 1000; count <= 9999; count += 1) {
+            String[] number = String.valueOf(count).split("");
+            int higherOrder = Integer.parseInt(number[0] + number[1]);
+            int lowerOrder = Integer.parseInt(number[2] + number[3]);
+            int sum = higherOrder + lowerOrder;
+            if (sum * sum == count) {
+                numbers += (count < 9999) ? count + ", " : count;
+            }
+        }
+
+        System.out.println(numbers);
+
+        sc.close();   
+    }
+}
