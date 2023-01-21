8fb5cbdbb8c5e25c06f3163d291ad3a65371c987
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-21 12:56:00 -0300

    Exercise 910 - Arrays - Loiane Groner - done

--- Loiane-Grone/array-exercises/src/Exercise10.java
+++ Loiane-Grone/array-exercises/src/Exercise10.java
@@ -1,17 +1,15 @@
-public class Exercise9 {
+public class Exercise10 {
     public static void main(String[] args) throws Exception {
         int[] a = {5, 7, 7, 8, 9, 30, 55, 87, 93, 13};
-        int[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double[] c = new double[a.length];
+        int[] b = new int[a.length];
 
         for (int i = 0; i < a.length; i++) {
-            c[i] = a[i] / (float) (b[i]);
+            b[i] = (int) a[i] % 2;
         }
 
         for (int i = 0; i < a.length; i++) {
             System.out.println("A: " + a[i]);
-            System.out.println("B: " + b[i]);
-            System.out.println("C: " + c[i] + "\n");
+            System.out.println("B: " + b[i] + "\n");
         }
     }
 }
