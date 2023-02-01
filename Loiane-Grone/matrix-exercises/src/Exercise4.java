0e09e6c0dadacaac8e2b31dfd7b6aecafc5e6df0
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-01-31 21:04:28 -0300

    Exercise 4 - Matrices - Loiane Groner - done

---
+++ Loiane-Grone/matrix-exercises/src/Exercise4.java
@@ -0,0 +1,60 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise4 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int days = 30;
+        int hours = 24;
+        String[][] appointments = new String[days][hours];
+
+        System.out.println("Insert an appointent...");
+        do {
+            int day;
+            int hour;
+            System.out.println("Enter 1 to schedule an appointment.");
+            System.out.println("Enter 2 to check the appointment.");
+            System.out.print("- - - > ");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.print("Inform the day of the month (1-30): ");
+                    day = sc.nextInt();
+
+                    System.out.print("Inform the hour of the day (1-24): ");
+                    hour = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter the name of your appointment to the agenda: ");
+                    String appointment = sc.nextLine();
+
+                    appointments[day][hour] = appointment;
+                    break;
+                case 2:
+                    System.out.print("Inform the day of the month (1-30): ");
+                    day = sc.nextInt();
+
+                    System.out.print("Inform the hour of the day (1-24): ");
+                    hour = sc.nextInt();
+
+                    System.out.printf("Appointment on day %d at %d hours \n", day, hour);
+                    System.out.println(appointments[day][hour]);
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+                    break;
+            }
+
+            System.out.print("Do you want to continue? (yes or y to continue) ");
+            String answer = sc.next();
+            if (!(answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes"))) {
+                break;
+            }
+            
+        } while (true);
+
+        sc.close();
+    }
+}
