879adf170e674b72f6efbda2ffc52f47bb55140e
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-02-07 12:25:15 -0300

    Section 7 - Exercise 25 - Geek University, done.

---
+++ Geek-University/section7/src/Exercise25.java
@@ -0,0 +1,30 @@
+import java.util.Locale;
+// import java.util.Scanner;
+
+public class Exercise25 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        // Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[100];
+        int count = 0;
+        int number = 1;
+
+        while (count < 100) {
+            String numberString = String.valueOf(number);
+            char lastDigit = numberString.charAt(numberString.length() - 1);
+            if (number % 7 != 0 && lastDigit != '7') {
+                numbers[count] = number;
+                count += 1;
+            }
+            number += 1;
+        }
+
+        System.out.println("Length: " + numbers.length);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        // sc.close();
+    }
+}
