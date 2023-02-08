5157e385b20322cb63eee16d6c5c29a29a8fa6ce
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-02-08 19:07:54 -0300

    Section 7 - Exercise 30 rectored - Geek University, done.

--- Geek-University/section7/src/Exercise30.java
+++ Geek-University/section7/src/Exercise30.java
@@ -19,19 +19,6 @@ public class Exercise30 {
             v2[i] = sc.nextInt();
         }
 
-        // while (true) {
-        //     int pos = 0;
-        //     for (int i = 0; i < v1.length; i++) {
-        //         aux[pos] = v1[i];
-        //         pos += 1;
-        //     }
-        //     for (int i = 0; i < v2.length; i++) {
-        //         aux[pos] = v2[i];
-        //         pos += 1;
-        //     }
-        //     break;
-        // }
-
         for (int i = 0; i < v1.length; i++) {
             for (int j = 0; j < v2.length; j++) {
                 if (v1[i] == v2[j]) {
