43f2ae8c4afd8ef9b84491f66959422acd3adb09
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-02-17 23:38:54 -0300

    Exercise 4 base - OOP - Loiane Groner - done

---
+++ Loiane-Grone/oop-exercises/src/exercise4/Exercise4.java
@@ -0,0 +1,22 @@
+package exercise4;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        LibraryBook book = new LibraryBook();
+        book.title = "Java®: Como Programar";
+        book.publisher = "Pearson Universidades; 10ª edição (24 junho 2016)";
+        book.language = "Portuguese";
+        book.isbn10 = "8543004799";
+        book.isbn13 = "978-8543004792";
+        book.dimensions = "28 x 20.8 x 3.8 cm";
+        book.rating = 5;
+        book.retningDate = new Date();
+
+        System.out.println("Books tile: " + book.title);
+    }
+}
---
+++ Loiane-Grone/oop-exercises/src/exercise4/LibraryBook.java
@@ -0,0 +1,15 @@
+package exercise4;
+
+import java.util.Date;
+
+public class LibraryBook {
+    
+    String title;
+    String publisher;
+    String language;
+    String isbn10;
+    String isbn13;
+    String dimensions;
+    int rating;
+    Date retningDate;
+}
