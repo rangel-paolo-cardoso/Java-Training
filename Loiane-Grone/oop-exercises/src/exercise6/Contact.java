9851bdcd87c11acdfca4412e647f956e030cd098
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-02-18 23:12:35 -0300

    Exercise 6 - OOP - Loiane Groner - done

---
+++ Loiane-Grone/oop-exercises/src/exercise6/Contact.java
@@ -0,0 +1,11 @@
+package exercise6;
+
+public class Contact {
+    
+    String phoneNumber;
+    String email;
+    String address;
+    String birthDay;
+    String note;
+    String nickName;
+}
---
+++ Loiane-Grone/oop-exercises/src/exercise6/Exercise6.java
@@ -0,0 +1,29 @@
+package exercise6;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Exercise6 {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Contact contact = new Contact();
+
+        contact.phoneNumber = "+00 (11) 0 0000-0000";
+        contact.email = "exercise6@outlook.com";
+        contact.address = "St. Code, N 23, 00-478";
+        contact.birthDay = format.format(format.parse("05/07/2002"));
+        contact.note = "I code Java";
+        contact.nickName = "Javer";
+
+        System.out.println("Phone number: " + contact.phoneNumber);
+        System.out.println("Email: " + contact.email);
+        System.out.println("Address: " + contact.address);
+        System.out.println("Birth day: " + contact.birthDay);
+        System.out.println("Note: " + contact.note);
+        System.out.println("Nickname: " + contact.nickName);
+    }
+}
