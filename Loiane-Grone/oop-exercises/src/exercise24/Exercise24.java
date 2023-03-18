e8ff05d5b221b812c3e37fe71e1fafbe0ed327fc
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-03-17 23:40:41 -0300

    Exercise 24 - Created SpecialAccount Class - OOP - Loiane Groner - done

--- Loiane-Grone/oop-exercises/src/exercise24/Exercise24.java
+++ Loiane-Grone/oop-exercises/src/exercise24/Exercise24.java
@@ -1,13 +1,10 @@
 package exercise24;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise24 {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
---
+++ Loiane-Grone/oop-exercises/src/exercise24/SpecialAccount.java
@@ -0,0 +1,39 @@
+package exercise24;
+
+public class SpecialAccount extends BankingAccount {
+    
+    private double limit;
+
+    public SpecialAccount() {}
+
+    public SpecialAccount(double limit) {
+        this.limit = limit;
+    }
+
+    public SpecialAccount(String customerName, int accountNumber, double balance, double limit) {
+        super(customerName, accountNumber, balance);
+        this.limit = limit;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public void withDraw(double amount) throws Exception {
+        if (this.getLimit() + this.getBalance() < amount) {
+            throw new Exception("Insufficient funds and limit!");
+        } else {
+            if (this.getBalance() < amount) {
+                double aux = amount - this.getBalance();
+                this.setBalance(this.getBalance() - this.getBalance());
+                this.setLimit(this.getLimit() - aux);
+            } else {
+                this.setBalance(this.getBalance() - amount);
+            }
+        }
+    }
+}
