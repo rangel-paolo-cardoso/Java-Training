23084a60a23d59c58a6e61816376821e653458ab
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-03-25 20:40:48 -0300

    Section 12 - Exercise 2 - Geek University, done.

---
+++ Geek-University/section12/src/exercise2/Exercise2.java
@@ -0,0 +1,23 @@
+package exercise2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the square side size: ");
+        double size = sc.nextDouble();
+
+        Square square = new Square(size);
+
+        square.calculateArea();
+        square.calculatePerimeter();
+
+        System.out.println(square.printInfo());
+
+        sc.close();
+    }
+}
---
+++ Geek-University/section12/src/exercise2/Square.java
@@ -0,0 +1,38 @@
+package exercise2;
+
+public class Square {
+    private double side;
+    private double area;
+    private double perimeter;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public void calculateArea() {
+        this.area = this.side * this.side;
+    }
+
+    public void calculatePerimeter() {
+        this.perimeter = this.side * 4;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public String printInfo() {
+        return "\n"
+                + "Side: " + this.side
+                + ",\nArea: " + this.area
+                + ",\nPerimeter: " + this.perimeter
+                + "\n";
+    }
+}
