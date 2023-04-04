5b6aaaede4f76d025c42e183c8d00269aff00e56
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-04-03 23:52:30 -0300

        Java OOP - Enums - EnumSet usuage - Baeldung

--- Baeldung/java_oop/enums/src/main/java/enums/Pizza.java
+++ Baeldung/java_oop/enums/src/main/java/enums/Pizza.java
@@ -1,6 +1,13 @@
 package enums;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Pizza {
+    private static EnumSet<PizzaStatus> undeliverablePizzaStatuses = EnumSet.of(PizzaStatus.ORDERED,
+            PizzaStatus.READY);
+
     private PizzaStatus status;
 
     public boolean isDeliverable() {
@@ -9,6 +16,23 @@ public class Pizza {
         }
         return false;
     }
+
+    public void printTimeToDeliver() {
+        System.out.println("Time to delivery is " +
+                this.geStatus().getTimeToDelivery() + " days");
+    }
+
+    public static List<Pizza> getAllUndeliverablePizzas(List<Pizza> input) {
+        return input.stream().filter(
+                (s) -> undeliverablePizzaStatuses.contains(s.geStatus()))
+                .collect(Collectors.toList());
+    }
+
+    // public void deliver() {
+    //     if (isDeliverable()) {
+    //         PizzaDeliverySystemConfiguration.getInstance()
+    //     }
+    // }
 
     public PizzaStatus geStatus() {
         return status;
--- Baeldung/java_oop/enums/src/test/java/enums/PizzaTest.java
+++ Baeldung/java_oop/enums/src/test/java/enums/PizzaTest.java
@@ -2,6 +2,9 @@ package enums;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PizzaTest {
@@ -11,5 +14,29 @@ public class PizzaTest {
         Pizza testPz = new Pizza();
         testPz.setStatus(PizzaStatus.READY);
         assertTrue(testPz.isDeliverable());
+    }
+
+    @Test
+    public void givenPizzaOrders_whenRetriveingUnDeliveredPzs_thenCorrectlyRetrieved() {
+        List<Pizza> pzList = new ArrayList<>();
+        Pizza pz1 = new Pizza();
+        pz1.setStatus(PizzaStatus.DELIVERED);
+
+        Pizza pz2 = new Pizza();
+        pz1.setStatus(PizzaStatus.ORDERED);
+
+        Pizza pz3 = new Pizza();
+        pz1.setStatus(PizzaStatus.ORDERED);
+
+        Pizza pz4 = new Pizza();
+        pz1.setStatus(PizzaStatus.READY);
+
+        pzList.add(pz1);
+        pzList.add(pz2);
+        pzList.add(pz3);
+        pzList.add(pz4);
+
+        List<Pizza> undeliverabledPzs = Pizza.getAllUndeliverablePizzas(pzList);
+        assertTrue(undeliverabledPzs.size() == 3);
     }
 }
