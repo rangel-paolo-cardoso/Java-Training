95732651b167890c8cbc9be9635fe504ebb7d217
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-04-05 23:56:20 -0300

    Java OOP - Enums - EnumMap usuage - Baeldung

--- Baeldung/java_oop/enums/src/main/java/enums/Pizza.java
+++ Baeldung/java_oop/enums/src/main/java/enums/Pizza.java
@@ -1,5 +1,7 @@
 package enums;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,6 +29,24 @@ public class Pizza {
                 (s) -> undeliverablePizzaStatuses.contains(s.geStatus()))
                 .collect(Collectors.toList());
     }
+
+    public static EnumMap<PizzaStatus, List<Pizza>>
+        groupPizzaByStatus(List<Pizza> pizzaList) {
+            EnumMap<PizzaStatus, List<Pizza>> pzByStatus =
+            new EnumMap<>(PizzaStatus.class);
+
+            for (Pizza pz : pizzaList) {
+                PizzaStatus status = pz.geStatus();
+                if (pzByStatus.containsKey(status)) {
+                    pzByStatus.get(status).add(pz);
+                } else {
+                    List<Pizza> newPzList = new ArrayList<>();
+                    newPzList.add(pz);
+                    pzByStatus.put(status, newPzList);
+                }
+            }
+            return pzByStatus;
+        }
 
     // public void deliver() {
     //     if (isDeliverable()) {
--- Baeldung/java_oop/enums/src/test/java/enums/PizzaTest.java
+++ Baeldung/java_oop/enums/src/test/java/enums/PizzaTest.java
@@ -3,12 +3,13 @@ package enums;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
 
 import org.junit.Test;
 
 public class PizzaTest {
-    
+
     @Test
     public void givenPizzaOrder_whenReady_thenDelivarable() {
         Pizza testPz = new Pizza();
@@ -38,5 +39,31 @@ public class PizzaTest {
 
         List<Pizza> undeliverabledPzs = Pizza.getAllUndeliverablePizzas(pzList);
         assertTrue(undeliverabledPzs.size() == 3);
+    }
+
+    @Test
+    public void givenPizaOrders_whenGroupByStatusCalled_thenCorrectlyGrouped() {
+        List<Pizza> pzList = new ArrayList<>();
+        Pizza pz1 = new Pizza();
+        pz1.setStatus(PizzaStatus.DELIVERED);
+
+        Pizza pz2 = new Pizza();
+        pz2.setStatus(PizzaStatus.ORDERED);
+
+        Pizza pz3 = new Pizza();
+        pz3.setStatus(PizzaStatus.ORDERED);
+
+        Pizza pz4 = new Pizza();
+        pz4.setStatus(PizzaStatus.READY);
+
+        pzList.add(pz1);
+        pzList.add(pz2);
+        pzList.add(pz3);
+        pzList.add(pz4);
+
+        EnumMap<PizzaStatus, List<Pizza>> map = Pizza.groupPizzaByStatus(pzList);
+        assertTrue(map.get(PizzaStatus.DELIVERED).size() == 1);
+        assertTrue(map.get(PizzaStatus.ORDERED).size() == 2);
+        assertTrue(map.get(PizzaStatus.READY).size() == 1);
     }
 }
