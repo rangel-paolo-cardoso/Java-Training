ff914057bf0d671dc66c915c6bef3cc4ae347b09
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-04-09 00:08:07 -0300

    Java String - All About String - Generate Random Alphanumeric using Java8 - Baeldung

--- Baeldung/all_about_string/random_string/src/test/java/generate_random_string/AppTest.java
+++ Baeldung/all_about_string/random_string/src/test/java/generate_random_string/AppTest.java
@@ -3,9 +3,23 @@ package generate_random_string;
 import java.nio.charset.Charset;
 import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
+
+    private int leftLimit;
+    private int rightLimit;
+    private int targetStringLength;
+    private Random random;
+
+    @Before
+    public void setUp() {
+        leftLimit = 97;
+        rightLimit = 122;
+        targetStringLength = 10;
+        random = new Random();
+    }
 
     @Test
     public void givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrect() {
@@ -18,10 +32,7 @@ public class AppTest {
 
     @Test
     public void givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrectAgain() {
-        int leftLimit = 97;
-        int rightLimit = 122;
-        int targetStringLength = 10;
-        Random random = new Random();
+        random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
             int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
@@ -34,15 +45,28 @@ public class AppTest {
 
     @Test
     public void givenUsingJava8_whenGeneratingRandomAlphabeticString_thenCorrect() {
-        int leftLimit = 97;
-        int rightLimit = 122;
-        int targetStringLength = 10;
-        Random random = new Random();
+        random = new Random();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
-            .limit(targetStringLength)
-            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-            .toString();
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(generatedString);
+    }
+
+    @Test
+    public void givenUsingJava8_whenGeneratingRandomAlphanumericString_thenCorrect() {
+        leftLimit = 48;
+        rightLimit = 122;
+        targetStringLength = 10;
+        random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
 
         System.out.println(generatedString);
     }
