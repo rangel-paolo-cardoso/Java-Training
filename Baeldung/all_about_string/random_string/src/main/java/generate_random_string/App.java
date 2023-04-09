0f923b371c40a7f37cb209b2dcde283d63d66be2
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-04-09 00:17:58 -0300

    Java String - All About String - Generate Random String using SecureRandom - Baeldung

--- Baeldung/all_about_string/random_string/src/main/java/generate_random_string/App.java
+++ Baeldung/all_about_string/random_string/src/main/java/generate_random_string/App.java
@@ -1,8 +1,5 @@
 package generate_random_string;
 
-import java.nio.charset.Charset;
-import java.util.Random;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
--- Baeldung/all_about_string/random_string/src/test/java/generate_random_string/AppTest.java
+++ Baeldung/all_about_string/random_string/src/test/java/generate_random_string/AppTest.java
@@ -1,6 +1,7 @@
 package generate_random_string;
 
 import java.nio.charset.Charset;
+import java.security.SecureRandom;
 import java.util.Random;
 
 import org.junit.Before;
@@ -11,14 +12,14 @@ public class AppTest {
     private int leftLimit;
     private int rightLimit;
     private int targetStringLength;
-    private Random random;
+    private SecureRandom random;
 
     @Before
     public void setUp() {
         leftLimit = 97;
         rightLimit = 122;
         targetStringLength = 10;
-        random = new Random();
+        random = new SecureRandom();
     }
 
     @Test
@@ -32,7 +33,7 @@ public class AppTest {
 
     @Test
     public void givenUsingPlainJava_whenGeneratingRandomStringUnbounded_thenCorrectAgain() {
-        random = new Random();
+        random = new SecureRandom();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
             int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
@@ -45,7 +46,7 @@ public class AppTest {
 
     @Test
     public void givenUsingJava8_whenGeneratingRandomAlphabeticString_thenCorrect() {
-        random = new Random();
+        random = new SecureRandom();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
                 .limit(targetStringLength)
@@ -60,7 +61,7 @@ public class AppTest {
         leftLimit = 48;
         rightLimit = 122;
         targetStringLength = 10;
-        random = new Random();
+        random = new SecureRandom();
 
         String generatedString = random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
