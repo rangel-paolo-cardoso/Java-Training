7078163f7a5c73c82cad3f0efca2b07f8bd88a47
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-09-04 23:57:37 -0300

    Almost ready

---
+++ Geek-University/section18/src/exercise2/Exercise2.java
@@ -0,0 +1,43 @@
+package exercise2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exercise2 {
+    
+    public static void main(String[] args) {
+        Scanner sc = getScanner();
+
+        System.out.print("Enter the path to the file: ");
+        String filePath = sc.nextLine();
+
+        try {
+            sc = getScanner(filePath);
+            int numberOfLines = countLines(sc);
+            System.out.printf("The file you enter has %d lines%n", numberOfLines);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("It was not possible to read the file.");
+        } finally {
+            sc.close();
+        }
+    }
+
+    private static int countLines(Scanner sc) {
+        int count = 0;
+        while (sc.hasNextLine()) {
+            count += 1;
+            System.out.println("count: " + count);
+        }
+        return count;
+    }
+
+    private static Scanner getScanner(String path) throws FileNotFoundException {
+        return new Scanner(new FileInputStream(path));
+    }
+
+    private static Scanner getScanner() {
+        return new Scanner(System.in);
+    }
+}
