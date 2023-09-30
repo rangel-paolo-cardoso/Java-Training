98d5a64c25df058b93172d3d22510e4e7a9dd502
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-09-30 20:29:43 -0300

    Section 18 - Exercise 24 - Geek University, improved the program's usability.

--- Geek-University/section18/src/exercise24/Exercise24.java
+++ Geek-University/section18/src/exercise24/Exercise24.java
@@ -1,7 +1,9 @@
 package exercise24;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -13,49 +15,27 @@ import java.util.Scanner;
  */
 public class Exercise24 {
 
-    public static void main(String[] args) {
-        Scanner sc = getScanner();
+    private static StringBuilder productInfoContainer = new StringBuilder();
+    private static Scanner sc = getScanner();
+    private static byte aux = 0;
 
-        byte aux = 0;
-        StringBuilder productInfoContainer = new StringBuilder();
-        int productCode;
-        String productDescription;
-        int productQuantity;
+    public static void main(String[] args) {
+
+        boolean finishExecution = false;
         do {
             printMenu();
-            char option = sc.nextLine().toLowerCase().charAt(0);
-            if (option != 'y') {
+            executeOption();
+            System.out.println("Do you want to finish now? (Attention: If you did not save, the information will be lost)");
+            System.out.print("Enter Y (Yes) or any another key to continue: ");
+            finishExecution = sc.nextLine().toLowerCase().charAt(0) == 'y' ? true : false;
+            if (finishExecution) {
                 break;
             }
-            if (aux > 0) {
-                productInfoContainer.append("\n");
-            }
-            System.out.print("Enter the product's code: ");
-            productCode = sc.nextInt();
-            sc.nextLine();
-
-            System.out.print("Enter the product's descritpion: ");
-            productDescription = sc.nextLine();
-
-            System.out.print("Enter the product's quantity: ");
-            productQuantity = sc.nextInt();
-            sc.nextLine();
-
-            addPorductInfoToTheInfoContainer(
-                    productInfoContainer,
-                    productCode,
-                    productDescription,
-                    productQuantity);
-            if (aux == 0) {
-                aux += 1;
-            }
         } while (true);
-
-        saveInfoToTheFile(productInfoContainer, sc);
     }
 
-    private static void saveInfoToTheFile(StringBuilder productInfoContainer, Scanner sc) {
-        String fileNameAndPath = sc.nextLine();
+    private static void saveInfoToTheFile() {
+        String fileNameAndPath = "./src/exercise24/grocery-store.bin";
         try (PrintStream fileWriter = new PrintStream(fileNameAndPath)) {
             fileWriter.print(productInfoContainer.toString());
         } catch (FileNotFoundException e) {
@@ -64,18 +44,67 @@ public class Exercise24 {
         }
     }
 
-    private static void addPorductInfoToTheInfoContainer(
-            StringBuilder productInfoContainer,
-            int productCode,
-            String productDescription, int productQuantity) {
+    private static void addProductInformation() {
+        if (aux == 1) {
+            productInfoContainer.append("\n");
+        }
+        System.out.print("Enter the product's code: ");
+        int productCode = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter the product's descritpion: ");
+        String productDescription = sc.nextLine();
+
+        System.out.print("Enter the product's quantity: ");
+        int productQuantity = sc.nextInt();
+        sc.nextLine();
+
         productInfoContainer.append("Code: " + productCode);
         productInfoContainer.append(", description: " + productDescription);
         productInfoContainer.append(", quantity: " + productQuantity);
+        if (aux == 0) {
+            aux += 1;
+        }
+    }
+
+    private static void executeOption() {
+        systemLoop: {
+            while (true) {
+                char option = sc.nextLine().toLowerCase().charAt(0);
+
+                switch (option) {
+                    case '1':
+                        addProductInformation();
+                        break systemLoop;
+                    case '2':
+                        System.out.println();
+                        break systemLoop;
+                    case '3':
+                        saveInfoToTheFile();
+                        break systemLoop;
+                    case '4':
+                        System.out.println();
+                        break systemLoop;
+                    case '5':
+                        System.out.println();
+                        break systemLoop;
+                    default:
+                        System.out.println("\nInvalid option! Try again.");
+                        printMenu();
+                }
+            }
+        }
     }
 
     private static void printMenu() {
-        System.out.print("Do you want to enter a product information?\n"
-                + "Enter Y (Yes) or press any other key to stop: ");
+        System.out.print(
+                "---------- Menu ----------\n"
+                        + "1 - Enter product information\n"
+                        + "2 - Remove a product certain product from the stock\n"
+                        + "3 - Save last updates\n"
+                        + "4 - Print general report\n"
+                        + "5 - Print report of non-available products\n"
+                        + "Choose your option: ");
     }
 
     private static Scanner getScanner() {
