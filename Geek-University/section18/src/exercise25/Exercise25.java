8e52d29fe8abadc65b8f548f4d85aad86dadcf64
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-10-05 22:40:49 -0300

    Section 18 - Exercise 24 - Geek University, added main functions' structure and a system loop for the menu.

--- Geek-University/section18/src/exercise25/Exercise25.java
+++ Geek-University/section18/src/exercise25/Exercise25.java
@@ -2,6 +2,8 @@ package exercise25;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -13,30 +15,93 @@ import java.util.Scanner;
  */
 public class Exercise25 {
 
+    private static StringBuilder contacts = new StringBuilder();
     private static Scanner sc = getScanner();
     private static String path = "./src/exercise25/contacts.txt";
-    
+
     public static void main(String[] args) {
 
         do {
             printMenu();
+            executeOption();
             break;
         } while (true);
-        
+
         sc.close();
+    }
+
+    private static void executeOption() {
+        systemLoop: {
+            while (true) {
+                char option = sc.nextLine().toLowerCase().charAt(0);
+                switch (option) {
+                    case '1':
+                        insertContactInfo();
+                        break systemLoop;
+                    case '2':
+                        removeContactInfo();
+                        break systemLoop;
+                    case '3':
+                        searchContactByName();
+                        break systemLoop;
+                    case '4':
+                        listAllContacts();
+                        break systemLoop;
+                    case '5':
+                        listContactsByFirstLetterName();
+                        break systemLoop;
+                    case '6':
+                        listContactsByBirthdaysOfCurrentMonth();
+                        break systemLoop;
+                    default:
+                        System.out.println("\nInvalid option! Try again.");
+                        printMenu();
+                }
+            }
+        }
+    }
+
+    private static void flushCachedContats() {
+        contacts.setLength(0);
+    }
+
+    private static void saveInfoToTheFile(boolean edit) {
+        try (PrintStream fileWriter = new PrintStream(new FileOutputStream(path, edit))) {
+            fileWriter.print(contacts.toString());
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured while trying to write info to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertContactInfo() {
+    }
+
+    private static void removeContactInfo() {
+    }
+
+    private static void searchContactByName() {
+    }
+
+    private static void listAllContacts() {
+    }
+
+    private static void listContactsByFirstLetterName() {
+    }
+
+    private static void listContactsByBirthdaysOfCurrentMonth() {
     }
 
     private static void printMenu() {
         System.out.print(
-            "=============== MENU ===============\n"
-            + "1 - Insert contact info\n"
-            + "2 - Remove contact info\n"
-            + "3 - Seacrh a contact by name\n"
-            + "4 - List all contacts"
-            + "5 - List all contact starting with a given letter"
-            + "6 - List everyone who have a birtday in the current month\n"
-            + "Enter your option: "
-        );
+                "=============== MENU ===============\n"
+                        + "1 - Insert contact info\n"
+                        + "2 - Remove contact info\n"
+                        + "3 - Seacrh a contact by name\n"
+                        + "4 - List all contacts"
+                        + "5 - List all contact starting with a given letter"
+                        + "6 - List everyone who have a birtday in the current month\n"
+                        + "Enter your option: ");
     }
 
     private static Scanner getScannerFileReader() throws FileNotFoundException {
