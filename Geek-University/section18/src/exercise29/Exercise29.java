9551746d9ee32b16e13e79de10923ab490de2cb2
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-10-24 23:44:53 -0300

    Section 18 - Exercise 29 - Geek University, implemented a method to create the file.

--- Geek-University/section18/src/exercise29/Exercise29.java
+++ Geek-University/section18/src/exercise29/Exercise29.java
@@ -1,12 +1,17 @@
 package exercise29;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercise29 {
 
+    private static String fileName = null;
     private static final String path = "./src/exercise29/";
     private static final Scanner sc = getScanner();
+    private static final StringBuilder fileContent = new StringBuilder();
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
@@ -41,8 +46,10 @@ public class Exercise29 {
                 char option = sc.nextLine().toLowerCase().charAt(0);
                 switch (option) {
                     case 1:
+                        createDataFile();
                         break systemLoop;
                     case 2:
+                        insertRecordToTheFile(true);
                         break systemLoop;
                     case 3:
                         break systemLoop;
@@ -62,6 +69,44 @@ public class Exercise29 {
             }
         }
         return exit;
+    }
+
+    private static void createDataFile() {
+        fileContent.setLength(0);
+        System.out.print("Enter the file name: ");
+        fileName = sc.nextLine();
+        writeContentToTheFile(false, path + fileName);
+    }
+
+    private static void insertRecordToTheFile(boolean append) {
+        System.out.print("Enter the seller code: ");
+        int sellerCode = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter the seller name: ");
+        String sellerName = sc.nextLine();
+
+        System.out.print("Enter the sale value: ");
+        float saleValue = sc.nextFloat();
+        sc.nextLine();
+
+        System.out.print("Enter the sale month: ");
+        byte saleMonth = sc.nextByte();
+        sc.nextLine();
+    }
+
+    private static void writeContentToTheFile(boolean append, String filePathAndName) {
+        try (PrintStream fileWriter = new PrintStream(new FileOutputStream(filePathAndName + ".txt", append))) {
+            fileWriter.println(fileContent.toString());
+            if (append) {
+                System.out.println("Data saved successfully!");
+            } else {
+                System.out.println("File created successfully!");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured while trying to save data to the disk.");
+            e.printStackTrace();
+        }
     }
 
     private static Scanner getScanner() {
